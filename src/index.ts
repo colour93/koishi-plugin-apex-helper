208d06e1cba1c6b307d27e37e7f11908dfad7379
FurryR <awathefox@gmail.com>
2023-05-19 23:23:44 +0800

    :art: 1st refactor

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,88 @@
 import { Context, Logger, Schema } from 'koishi'
 
-import { CraftingData, MapData } from './type'
+import {
+  BundleItem,
+  MapData,
+  duration,
+  new_crafting_data,
+  translate
+} from './type'
 
 export const name = 'apex-helper'
 
 export interface Config {
-	apikey: string
+  apikey: string
 }
 
 export const Config: Schema<Config> = Schema.object({
-	apikey: Schema.string().required().description("Apex Legends API 上的 API Key (https://portal.apexlegendsapi.com/)")
+  apikey: Schema.string()
+    .required()
+    .description(
+      'Apex Legends API 上的 API Key (https://portal.apexlegendsapi.com/)'
+    )
 })
 
-const apiBaseUrl = "https://api.mozambiquehe.re"
+const apiBaseUrl = 'https://api.mozambiquehe.re'
 
 export function apply(ctx: Context, config: Config) {
+  const logger = new Logger('apex-helper')
 
-	const logger = new Logger("apex-helper")
-	
-	ctx.command('apexhelper.map', "查询当前地图轮换")
-		.action(async (_) => {
+  logger.debug('Apex Legend helper started')
 
-			const map = await ctx.http.get(`${apiBaseUrl}/maprotation`, {
-				params: {
-					auth: config.apikey,
-					version: 2
-				}
-			}).catch(err => {throw err}).then(data => new MapData(data))
-			
-			if (!map) return "请求失败。"
+  ctx.command('apexhelper.map', '查询当前地图轮换').action(async _ => {
+    try {
+      const map = (await ctx.http.get(`${apiBaseUrl}/maprotation`, {
+        params: {
+          auth: config.apikey,
+          version: 2
+        }
+      })) as MapData
+      return (
+        `当前：${translate.event(map.battle_royale.current.map)}\n` +
+        `下个：${translate.event(map.battle_royale.next.map)}\n` +
+        `排位：${translate.event(map.ranked.current.map)}\n` +
+        `轮换：${map.battle_royale.current.remainingMins} 分钟后`
+      )
+    } catch (_) {
+      return '请求失败。'
+    }
+  })
 
-			return `当前：${map.battle_royale.current.map_zh}\n` + 
-				`下个：${map.battle_royale.next.map_zh}\n` +
-				`排位：${map.ranked.current.map_zh}\n` +
-				`轮换：${map.battle_royale.current.remainingMins} 分钟后`
-
-		})
-	
-	ctx.command('apexhelper.craft', "查询当前制造轮换")
-		.action(async (_) => {
-
-			const crafting = await ctx.http.get(`${apiBaseUrl}/crafting`, {
-				params: {
-					auth: config.apikey
-				}
-			}).catch(err => {throw err}).then(data => new CraftingData(data))
-			
-			if (!crafting) return "请求失败。"
-
-			return `每日：${crafting.daily.bundleContent.map(e => e.item_zh).join(' ')}\n` + 
-				`每周：${crafting.weekly.bundleContent.map(e => e.item_zh).join(' ')}\n` +
-				`武器：${crafting.weapon.bundleContent.map(e => e.item_zh).join(' ')}\n` +
-				`轮换：<i18n:time value="${crafting.daily.duration}" /> | <i18n:time value="${crafting.weekly.duration}" />`
-
-		})
-
+  ctx.command('apexhelper.craft', '查询当前制造轮换').action(async _ => {
+    try {
+      const crafting = new_crafting_data(
+        (await ctx.http.get(`${apiBaseUrl}/crafting`, {
+          params: {
+            auth: config.apikey
+          }
+        })) as BundleItem[]
+      )
+      const gen = (v: BundleItem) => {
+        return v.bundleContent.map(e => {
+          if (v.bundle == 'weapon') {
+            return translate.bundle.weapon(e)
+          }
+          return translate.bundle.common(e)
+        })
+      }
+      return (
+        `每日：${
+          crafting['daily'] ? gen(crafting['daily']).join(' ') : '未获取'
+        }\n` +
+        `每周：${
+          crafting['weekly'] ? gen(crafting['weekly']).join(' ') : '未获取'
+        }\n` +
+        `武器：${
+          crafting['weapon'] ? gen(crafting['weapon']).join(' ') : '未获取'
+        }\n` +
+        `轮换：<i18n:time value="${
+          crafting['daily'] ? duration(crafting['daily']) : '未获取'
+        }" /> | <i18n:time value="${
+          crafting['weekly'] ? duration(crafting['weekly']) : '未获取'
+        }" />`
+      )
+    } catch (_) {
+      return '请求失败。'
+    }
+  })
 }
